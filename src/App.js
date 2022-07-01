c2169b99a6cfa345f80f25b5d5256471a6e4e85f
Ivan Chan <ivanchan.wb@gmail.com>
2022-07-01 17:36:09 +1000

    Implemented basic countdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react'; 
 
 function App() {
-  const [timer, setTimer] = useState(0); 
+  const [timer, setTimer] = useState(60);
+  const [counting, setCounting] = useState(false);
 
   useEffect(() => {
-    setInterval(() => {
+    if (!counting) return;
+    setTimeout(() => {
       setTimer((timer) => timer - 1);
     }, 1000);
-  }, []); 
+  }, [timer, counting]); 
 
+  const startHandler = () => {
+    setTimer(60);
+    setCounting(true);
+  }
+  
   return (
     <div>
       {timer}
+      <button onClick={startHandler}>Start</button>
     </div>
   );
 }
