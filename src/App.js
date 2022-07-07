19fe68f2dd702cc434b35e1184c0b1574e2bfcab
Nikki Nguyen <mikk@mikk.localdomain>
2022-07-07 22:09:53 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import LoginPage from 'pages/LoginPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="login" element={<LoginPage />} />
+                    {/* <Route path="signup" element={<SignupPage />} /> */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <div>
+            <p>This is a login page.</p>
+        </div>
+    )
+}
+
+export default LoginPage;
