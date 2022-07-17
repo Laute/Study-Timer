0024add070c4a4dc82147ea3a36348317866d188
Nikki Nguyen <sumwon@192-168-1-109.tpgi.com.au>
2022-07-17 10:43:22 +1000

    implemented login & signup pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import ReactDOM from "react-dom/client";
 import {
     BrowserRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
-import LoginPage from 'pages/LoginPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
-                <Route path="/" element={<App />}>
-                    <Route path="login" element={<LoginPage />} />
-                    {/* <Route path="signup" element={<SignupPage />} /> */}
-                </Route>
+                <Route path="/" element={<App />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 }
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState, setState } from 'react';
+import Header from '../components/Header.js'
 
 function LoginPage() {
     return (
-        <div>
-            <p>This is a login page.</p>
+        <div class="login-page">
+            <Header />
+            <LoginBody />
+            <LoginForm />
         </div>
     )
 }
+
+
+function LoginBody() {
+    return (
+        <div class="login-body">
+            <div class="centred-body">
+                <h2>Welcome Back!</h2>
+            </div>
+            <div class="body">
+                <p>New to Pomodoro?</p>
+                <button class="btn-small">Create an account</button>
+            </div>
+        </div>
+    )
+}
+
+function LoginForm() {
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        if (id === "email") {
+            setEmail(value);
+        }
+        if (id === "password") {
+            setPassword(value);
+        }
+    }
+    const handleSubmit = () => {
+        console.log(email, password);
+    }
+    return (
+        <div class="form">
+            <form class="form-fields">
+                <label class="form-text">Email</label>
+                <div />
+                <input type="text" id="email" onChange={(e) => handleInputChange(e)} /><br />
+                <div />
+                <label class="form-text">Password</label>
+                <div />
+                <input type="text" id="password" onChange={(e) => handleInputChange(e)} />
+            </form>
+            <div class="footer">
+                <button onClick={() => handleSubmit()} type="submit" class="btn-large">Login</button>
+            </div>
+        </div>
+    )
+}
+
+
 
 export default LoginPage;
