8d46cea1371c8f5a3299c4838fbf9e7aa6dff26c
Ivan Chan <ivanchan.wb@gmail.com>
2022-07-25 00:04:16 +1000

    Centred timer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import { useState, useEffect } from 'react'; 
+import Timer from "./components/Timer";
 
 function App() {
-  const [timer, setTimer] = useState(60);
-  const [counting, setCounting] = useState(false);
-  const [studyLength, setStudyLength] = useState(0); 
-  const [breakLength, setBreakLength] = useState(0); 
-
-  useEffect(() => {
-    if (!counting) return;
-    setTimeout(() => {
-      setTimer((timer) => timer - 1);
-    }, 1000);
-  }, [timer, counting]); 
-
-  const startHandler = () => {
-    setTimer(studyLength);
-    setCounting(true);
-  }
-  
-  const handleStudyChange = (event) => {
-    setStudyLength(event.target.value);
-  }
-
-  const handleBreakChange = (event) => {
-    setBreakLength(event.target.value); 
-  }
-  
   return (
     <div>
-      {timer}
-      <button onClick={startHandler}>Start</button>
-      <input onChange={(event) => handleStudyChange(event)}></input>
-      <input onChange={(event) => handleBreakChange(event)}></input>
+      <center>
+        <Timer />
+      </center>
     </div>
   );
 }
