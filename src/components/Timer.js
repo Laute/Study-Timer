dd20f7ed5192dc1d6686ce368ea40b8d1c1b1ba4
Ivan Chan <ivanchan.wb@gmail.com>
2022-08-08 22:03:08 +1000

    Added skip button, study period indicator, labels and preset time buttons

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import './Timer.css';
 
 // total number of periods equals twice the number of study periods as there is
 // a break period for each study period
 const NUM_STUDY_PERIODS = 4;
-const FINAL_PERIOD = NUM_STUDY_PERIODS * 2;
+const FINAL_PERIOD = NUM_STUDY_PERIODS * 2 - 1;
 
 const Timer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -13,6 +14,7 @@ const Timer = () => {
   const [longBreakDuration, setLongBreakDuration] = useState(4);
   const [studyDuration, setStudyDuration] = useState(5);
   const [period, setPeriod] = useState(1);
+  const [dotIndex, setDotIndex] = useState(0); 
   const [colorsTime, setColorsTime] = useState(
     [1, 0.6, 0.3, 0].map((x) => studyDuration * x)
   );
@@ -67,6 +69,7 @@ const Timer = () => {
     setIsBreak((isBreak) => !isBreak);
     // incrementing period restarts the timer as it's the timer key
     setPeriod((period) => period + 1);
+    handleDot();
   };
 
   const handleStudyDuration = (event) => {
@@ -82,30 +85,81 @@ const Timer = () => {
   };
 
   const handleRestart = () => {
-    setIsPlaying((isPlaying) => !isPlaying);
-    setPeriod(0);
+    setIsPlaying(false);
+    setPeriod(1);
+    setDotIndex(0);
+    setIsBreak(false);
   };
 
+  const handleSkip = () => {
+    handleOnComplete(); 
+  }
+
+  const handleDot = () => {
+    if (period % 2 === 0) {
+      setDotIndex((index) => (index + 1)); 
+    }
+  }
+
   return (
-    <>
-      <CountdownCircleTimer
-        key={period}
-        isPlaying={isPlaying}
-        duration={isBreak ? breakDuration : studyDuration}
-        colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-        colorsTime={colorsTime}
-        onComplete={handleOnComplete}
-      >
-        {renderTime}
-      </CountdownCircleTimer>
-      <button onClick={() => setIsPlaying((isPlaying) => !isPlaying)}>
-        {isPlaying ? "Pause" : "Start"}
-      </button>
-      <input onChange={(event) => handleStudyDuration(event)}></input>
-      <input onChange={(event) => handleBreakDuration(event)}></input>
-      <input onChange={(event) => handleLongBreakDuration(event)}></input>
-      <button onClick={handleRestart}>Restart</button>
-    </>
+    <div className="timer-container">
+      <div className="countdown-circle">
+        <CountdownCircleTimer
+          key={period}
+          size={300}
+          strokeWidth={20}
+          trailStrokeWidth={12}
+          isPlaying={isPlaying}
+          duration={isBreak ? breakDuration : studyDuration}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={colorsTime}
+          onComplete={handleOnComplete}
+        >
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
+      <div className="controls"> 
+        <button className ="control-button" onClick={() => setIsPlaying((isPlaying) => !isPlaying)}>
+          {isPlaying ? "Pause" : "Start"}
+        </button>
+        <button className ="control-button" onClick={handleRestart}>Restart</button>
+        <button className ="control-button" onClick = {handleSkip}>Skip</button>
+      </div>
+
+      <div> 
+        {Array.from({length: 4}).map((item, index) => (
+          <div className={dotIndex === index ? "dot active" : "dot"}></div>
+        ))}
+      </div>
+      
+      <div className="timer-settings-container">
+        <p>Study Length</p>
+        <div className="button-group">
+          <button className="duration-button" onClick ={() => setStudyDuration(15 * 60)}>15</button>
+          <button className="duration-button" onClick ={() => setStudyDuration(25 * 60)}>25</button>
+          <button className="duration-button" onClick ={() => setStudyDuration(30 * 60)}>30</button>
+          <button className="duration-button" onClick ={() => setStudyDuration(45 * 60)}>45</button>
+          <button className="duration-button" onClick ={() => setStudyDuration(60 * 60)}>60</button>
+        </div>
+        <input className="time-input" type="number" min="1" onChange={(event) => handleStudyDuration(event)}></input>
+
+        <p>Break Length</p>
+        <div className="button-group">
+          <button className="duration-button" onClick ={() => setBreakDuration(5 * 60)}>5</button>
+          <button className="duration-button" onClick ={() => setBreakDuration(10 * 60)}>10</button>
+          <button className="duration-button" onClick ={() => setBreakDuration(15 * 60)}>15</button>
+        </div>
+        <input className="time-input" type="number" min="1" onChange={(event) => handleBreakDuration(event)}></input>
+
+        <p>Long Break Length</p>
+          <div className="button-group">
+            <button className="duration-button" onClick ={() => setLongBreakDuration(30 * 60)}>30</button>
+            <button className="duration-button" onClick ={() => setLongBreakDuration(45 * 60)}>45</button>
+            <button className="duration-button" onClick ={() => setLongBreakDuration(60 * 60)}>60</button>
+          </div>
+        <input className="time-input" type="number" min="1" onChange={(event) => handleLongBreakDuration(event)}></input>
+      </div>
+    </div>
   );
 };
 
