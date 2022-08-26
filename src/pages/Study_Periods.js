327719cd7290035e270eead05cfeb1d29a63d67b
Sohum Shah <sohumshah2@gmail.com>
2022-08-27 01:40:31 +1000

    fixed stuff

--- src/pages/Study_Periods.js
+++ src/pages/Study_Periods.js
@@ -16,14 +16,51 @@ const Avg_Per_Day = (props) => {
         display_value = props.year_value;
     }
 
-
     if (props.type === 'study_time') {
+
+        if (display_value >= 86400) {
+            const days = Math.floor(display_value / 86400);
+            const hours = Math.floor((display_value - (days * 86400)) / 3600);
+            const minutes = Math.floor((display_value - days * 86400 - hours * 3600) / 60);
+            const seconds = display_value - days * 86400 - hours * 3600 - minutes * 60;
+
+        return(
+            <h2 style={{
+            color: props.colour,
+            }}> {days} days, {hours} hours, {minutes} minutes, {seconds} seconds</h2>
+            )
+        }
+
+        if (display_value >= 3600) {
+            const hours = Math.floor(display_value / 3600);
+            const minutes = Math.floor((display_value - (hours * 3600)) / 60);
+            const seconds = display_value - hours * 3600 - minutes * 60;
+            
+            return(
+                <h2 style={{
+                color: props.colour,
+                }}> {hours} hours, {minutes} minutes, {seconds} seconds</h2>
+                )
+        }
+
+        if (display_value >= 60) {
+            const minutes = Math.floor(display_value / 60);
+            const seconds = display_value - minutes * 60;
+
+            return(
+                <h2 style={{
+                color: props.colour,
+                }}>{minutes} minutes, {seconds} seconds</h2>
+                )
+        }
+
     return(
         <h2 style={{
         color: props.colour,
-        }}>{display_value} minutes</h2>
+        }}>{display_value} seconds</h2>
         )
     }
+
     else {
         return(
             <h2 style={{
